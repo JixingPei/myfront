ae0efd24206a8f8d78634d356afed790920582cd
老裴 <1824752960@qq.com>
2020-07-31 20:24:06 +0800

    comite

--- src/views/photo/date/index.vue
+++ src/views/photo/date/index.vue
@@ -49,7 +49,7 @@ export default {
       imagesLists: [
         {
           imagesList: [
-            'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
+            'http://192.168.0.107:8888/img/20190612/CSC_0177.JPG',
             'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg'
           ]
         },
